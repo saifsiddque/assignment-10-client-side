7fa987b62f7a0573da127038f03fffbd0392b153
saif <abutalha666124@gmail.com>
2022-10-27 19:05:41 +0600

    finish

--- auth-router-context-app/src/firebase/firebase.config.js
+++ auth-router-context-app/src/firebase/firebase.config.js
@@ -5,12 +5,20 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCkEn_WmveSGry2xF59prpyR_LrXqiqKSc",
+  apiKey: "AIzaSyCkEn_WmveSGry2xF59prpyR_LrXqiqKSc" ,
   authDomain: "assignment-10-3df80.firebaseapp.com",
   projectId: "assignment-10-3df80",
   storageBucket: "assignment-10-3df80.appspot.com",
   messagingSenderId: "37207122609",
   appId: "1:37207122609:web:84c505c692b927623ef890"
+
+
+  // apiKey: process.env.REACT_APP_NO_apiKey,
+  // authDomain: process.env.REACT_APP_NO_authDomain,
+  // projectId: process.env.REACT_APP_NO_projectId,
+  // storageBucket: process.env.REACT_APP_NO_storageBucket,
+  // messagingSenderId: process.env.REACT_APP_NO_messagingSenderId,
+  // appId: process.env.REACT_APP_NO_appId
 };
 
 // Initialize Firebase
